25beaf9a8d6b420dcc748bb6c8cb9274743da1e4
shaheerkhan-21 <i211358@nu.edu.pk>
2024-10-06 21:49:42 +0500

    db

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,17 +1,18 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-mongoose.connect(process.env.mongo_url)
+// Directly using the MongoDB URI
+const mongoUrl = "mongodb+srv://khanshaheer6969:5yfiC8hdP0VfU3mM@cluster0.n28a9.mongodb.net/";
 
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 
-const connection = mongoose.connection
+const connection = mongoose.connection;
 
-connection.on("error",()=>{
-    console.log("Error is  in connection")
-})
+connection.on("error", () => {
+    console.log("Error is in connection");
+});
 
-connection.on("connected",()=>{
-    console.log("Connected")
-})
+connection.on("connected", () => {
+    console.log("Connected");
+});
 
-module.exports = connection
-
+module.exports = connection;
